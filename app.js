b7d7fe756a3b7ce4b693870ac58035d9ab3ff5af
mehtab2899 <mehtabmultani51@gmail.com>
2021-08-22 18:35:44 +0530

    fix:fixing delete todo bug

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-window.onload = function() {
-const addtodo = document.querySelector('.add');
-const list = document.querySelector('.todos');
-const search = document.querySelector('.search input');
+const addtodo = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+const deleteTodo = document.querySelector(".delete");
+const listItem = document.querySelector(".list-group-item");
 
-const generateTemplate = todo => {
-
-    const html = `
+const generateTemplate = (todo, index) => {
+  const html = `
         <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${todo}</span>
-        <i class="far fa-trash-alt delete"></i>
+        <i class="far fa-trash-alt delete" onclick="deleteTodoItem(${index})"></i>
         </li>
     `;
-    list.innerHTML += html;
+  list.innerHTML += html;
 };
 
-addtodo.addEventListener('submit', e =>{
-    e.preventDefault();
-    const todo = addtodo.add.value.trim();
-    
-    if(todo.length){
+addtodo.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addtodo.add.value.trim();
+
+  if (todo.length) {
     generateTemplate(todo);
     addtodo.reset();
-    }
+  }
 });
 
 // delete todos
-list.addEventListener('click', e => {
-
-    if(e.target.classList.contains('delete')) {
-        e.target.parentElement.remove();
-    }
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentNode.remove();
+  }
 });
 
 const filterTodos = (term) => {
-
-    Array.from(list.children)
+  Array.from(list.children)
     .filter((todo) => !todo.textContent.toLowerCase().includes(term))
-    .forEach((todo) => todo.classList.add('filtered'));
+    .forEach((todo) => todo.classList.add("filtered"));
 
-   Array.from(list.children)
-    .filter((todo) =>  todo.textContent.toLowerCase().includes(term))
-    .forEach((todo) => todo.classList.remove('filtered'));
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
 
-}; 
 //keyup event
-   search.addEventListener('keyup', () => {
-     const term = search.value.trim().toLowerCase();
-    filterTodos(term);
-    });
-
-}
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
